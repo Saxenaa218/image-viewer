8c5fd3f6aeca655f835b67480cf7ace3f07ad97b
Abhishek Saxena <saxenaa218@gmail.com>
2023-07-08 13:44:54 +0530

    Updates

---
+++ assets/ImageModal-8cf3b11c.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-3d918af1.js";const d=({activeImage:o,onClose:l})=>s.jsx("div",{className:"modal",children:s.jsxs("div",{className:"modal-content",children:[s.jsx("img",{src:o.download_url,alt:o.author}),s.jsx("button",{className:"modal-close",onClick:l,children:"close"})]})});export{d as default};
